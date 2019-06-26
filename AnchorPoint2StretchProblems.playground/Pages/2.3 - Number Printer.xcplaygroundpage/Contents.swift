ea86bc5b71e9370955177e74068bfb544cab91a0
JarWarren <gentlemanjaruna@gmail.com>
2019-06-26 12:07:02 -0600

    adds 2.3

---
+++ AnchorPoint2StretchProblems.playground/Pages/2.3 - Number Printer.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+//: [Previous](@previous)
+/*:
+ # Wednesday Stretch Problem 2.3
+ ## Number Printer
+ ### Instructions
+ - Create a function that takes a single digit `Int` as a parameter and prints it to the console the way an old-school alarm clock would look. You may create auxiliary functions if needed.
+ - For example, if you pass 2 the console might look something like this:
+ ```
+  ---
+     |
+  ---
+ |
+  ---
+ ```
+ - If you pass 8 the console might look something like this:
+ ```
+  ---
+ |   |
+  ---
+ |   |
+  ---
+ ```
+ ### Black Diamond 💎💎💎
+ - Modify the function to take an `Int` with multiple digits and print each number in the same fashion on a single Line.
+ - For example, if you pass 257 the console might look something like this:
+ ```
+  ---   ---  ---
+     | |        |
+  ---   ---     |
+ |         |    |
+  ---   ---     |
+```
+ */
+import Foundation
+
+
+
+
+
+
+
+//: [Next](@next)
